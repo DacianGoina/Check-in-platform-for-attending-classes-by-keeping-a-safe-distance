537c56672345c3628e40af3e1c3a872542872266
dacianG <dacian.goina00@e-uvt.ro>
2021-07-29 10:43:05 +0300

    modificari planners

--- src/main/java/com/example/springbackend/model/Planner.java
+++ src/main/java/com/example/springbackend/model/Planner.java
@@ -10,7 +10,9 @@ import java.sql.Time;
 public class Planner {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "CLASSROOM_ID", nullable = false, updatable = false)
+    @Column(name = "ID")
+    private Long id;
+    @Column(name = "CLASSROOM_ID")
     private Long classroomId;
 
     @Column(name = "CLASS_ID", nullable = false, updatable = false)
@@ -29,7 +31,8 @@ public class Planner {
 
     }
 
-    public Planner(Long classroomId, Long classId, Time startTime, Time endTime, Date date) {
+    public Planner(Long id, Long classroomId, Long classId, Time startTime, Time endTime, Date date) {
+        this.id = id;
         this.classroomId = classroomId;
         this.classId = classId;
         this.startTime = startTime;
@@ -37,51 +40,48 @@ public class Planner {
         this.date = date;
     }
 
-    @Override
-    public String toString() {
-        return "Planner{" +
-                "classroomId=" + classroomId +
-                ", classId=" + classId +
-                ", startTime=" + startTime +
-                ", endTime=" + endTime +
-                ", date=" + date +
-                '}';
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public Long getClassroomId() {
         return classroomId;
     }
 
-    public Long getClassId() {
-        return classId;
-    }
-
-    public Time getStartTime() {
-        return startTime;
-    }
-
-    public Time getEndTime() {
-        return endTime;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
     public void setClassroomId(Long classroomId) {
         this.classroomId = classroomId;
+    }
+
+    public Long getClassId() {
+        return classId;
     }
 
     public void setClassId(Long classId) {
         this.classId = classId;
     }
 
+    public Time getStartTime() {
+        return startTime;
+    }
+
     public void setStartTime(Time startTime) {
         this.startTime = startTime;
     }
 
+    public Time getEndTime() {
+        return endTime;
+    }
+
     public void setEndTime(Time endTime) {
         this.endTime = endTime;
+    }
+
+    public Date getDate() {
+        return date;
     }
 
     public void setDate(Date date) {
