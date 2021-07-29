34ce58d12ddcd739c2ad81d7a4f3e13f57c46064
dacianG <dacian.goina00@e-uvt.ro>
2021-07-29 11:46:10 +0300

    modificari class-details

--- src/main/java/com/example/springbackend/dto/ClassDetails.java
+++ src/main/java/com/example/springbackend/dto/ClassDetails.java
@@ -1,5 +1,7 @@
 package com.example.springbackend.dto;
 
+import com.example.springbackend.model.Classroom;
+
 import javax.persistence.*;
 import java.sql.Date;
 import java.sql.Time;
@@ -15,6 +17,9 @@ public class ClassDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
     @Column(name = "CLASSROOM_ID")
     private Long classroomID;
     @Column(name = "CLASS_ID")
@@ -28,113 +33,31 @@ public class ClassDetails {
     private Date data;
 
 
-    /*@JoinTable(
+
+    /*
+    @JoinTable(
             name="CLASSROOMS",
-            joinColumns=@JoinColumn(name="NAME", referencedColumnName="CLASSROOM_ID"))*/
+            joinColumns=@JoinColumn(name="NAME", referencedColumnName="CLASSROOM_ID"))
     @Column(name = "NAME", table = "CLASSROOMS")
     private String roomName;
 
 
-    /*@JoinTable(
+    @JoinTable(
             name="CLASSES",
-            joinColumns=@JoinColumn(name="NAME", referencedColumnName="CLASS_ID"))*/
+            joinColumns=@JoinColumn(name="NAME", referencedColumnName="CLASS_ID"))
     @Column(name = "NAME", table = "CLASSES")
     private String courseName;
 
     @Column(name="CAPACITY",table = "CLASSROOMS")
     private Integer capacity;
 
-    //@Column(name = "LASTNAME", table = "USERS2")
-    //private String teacherFirstName;
 
-    @Column(name = "TEACHER_ID", table = "CLASSES", updatable = false, insertable = false)
+    @Column(name = "TEACHER_ID", table = "CLASSES")
     private Integer teacherID;
-
+    */
     public ClassDetails(){
 
     }
 
-    public ClassDetails(Long classroomID, Long classID, Time startTime, Time endTime, Date data, String roomName, String courseName, Integer capacity, Integer teacherID) {
-        this.classroomID = classroomID;
-        this.classID = classID;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.data = data;
-        this.roomName = roomName;
-        this.courseName = courseName;
-        this.capacity = capacity;
-        this.teacherID = teacherID;
-    }
 
-    public Long getClassroomID() {
-        return classroomID;
-    }
-
-    public Long getClassID() {
-        return classID;
-    }
-
-    public Time getStartTime() {
-        return startTime;
-    }
-
-    public Time getEndTime() {
-        return endTime;
-    }
-
-    public Date getData() {
-        return data;
-    }
-
-    public String getRoomName() {
-        return roomName;
-    }
-
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public void setClassroomID(Long classroomID) {
-        this.classroomID = classroomID;
-    }
-
-    public void setClassID(Long classID) {
-        this.classID = classID;
-    }
-
-    public void setStartTime(Time startTime) {
-        this.startTime = startTime;
-    }
-
-    public void setEndTime(Time endTime) {
-        this.endTime = endTime;
-    }
-
-    public void setData(Date data) {
-        this.data = data;
-    }
-
-    public void setRoomName(String roomName) {
-        this.roomName = roomName;
-    }
-
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
-    public Integer getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(Integer capacity) {
-        this.capacity = capacity;
-    }
-
-    public Integer getTeacherID() {
-        return teacherID;
-    }
-
-    public void setTeacherID(Integer teacherID) {
-        this.teacherID = teacherID;
-    }
 }
